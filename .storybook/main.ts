d420d814da0393f6b9e6be85700e6d141f1b24d5
Dmitrii Kuznetsov <dk.redu@gmail.com>
2024-03-10 19:14:48 +0000

    move story to component

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,20 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-    stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
     addons: [
         '@storybook/addon-links',
         '@storybook/addon-essentials',
         '@storybook/addon-onboarding',
         '@storybook/addon-interactions',
-        '@storybook/addon-a11y'
+        '@storybook/addon-a11y',
     ],
+    docs: {
+        autodocs: 'tag',
+    },
     framework: {
         name: '@storybook/react-vite',
         options: {},
     },
-    docs: {
-        autodocs: 'tag',
-    },
+    stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 };
 export default config;
